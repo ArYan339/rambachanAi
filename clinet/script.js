75c113f75abe41377bd2abd4ea7c693501bb8de2
ArYan339 <117717585+ArYan339@users.noreply.github.com>
2023-04-05 04:32:52 -0500

    Updated the server URL

--- clinet/script.js
+++ clinet/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://coddey.vercel.app/', {
+    const response = await fetch('https://codey-1o90.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
