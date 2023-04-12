052a9fc195fdab08611e5141604884aaf0f81fe0
ArYan Si <117717585+ArYan339@users.noreply.github.com>
2023-04-12 06:13:36 -0500

    Update script.js

--- clinet/script.js
+++ clinet/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codey-1o90.onrender.com', {
+    const response = await fetch('https://codey-edbn.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
